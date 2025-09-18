0bccf5f45dc1815526303552bc5336f58fa98fc1
VandanKambodi <vandankambodi27@email.com>
2025-09-18 16:10:52 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Ignore ESLint errors during Vercel builds
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  // Ignore TypeScript type errors during builds
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
